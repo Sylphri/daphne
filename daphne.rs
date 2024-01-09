6448804cbc526a3fcc2fad5f57e79dec13008fc4
Sylphri <sylphri@gmail.com>
2024-01-09 20:04:18 +0200

    Implementation of parentheses

--- daphne.rs
+++ daphne.rs
@@ -5,17 +5,20 @@ mod external;
 
 use external::{TermSize, get_terminal_size};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum OpType {
     Add,
     Sub,
     Mul,
     Div,
+    Pow,
     UnaryPlus,
     UnaryMinus,
+    LeftParen,
+    RightParen,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum TokenType {
     Number(f64),
     Operation(OpType),
@@ -39,6 +42,13 @@ fn is_operation(token: &Token) -> bool {
     }
 }
 
+fn is_number(token: &Token) -> bool {
+    match token.ttype {
+        TokenType::Number(_) => true,
+        _ => false,
+    }
+}
+
 fn is_numeric(byte: u8) -> bool {
     (b'0'..=b'9').contains(&byte) || byte == b'.'
 }
@@ -57,7 +67,7 @@ fn trim_start(line: &str) -> (&str, usize) {
 }
 
 fn parse(input: &str) -> Result<Vec<Token>, ParseErr> {
-    let operators = &['+', '-', '*', '/'];
+    let operators = &['+', '-', '*', '/', '^', '(', ')'];
     let mut tokens = vec![];
     let (mut input, mut begin) = trim_start(input);
     while let Some(i) = input.find(|c: char| c.is_ascii_whitespace()) {
@@ -91,6 +101,9 @@ fn parse(input: &str) -> Result<Vec<Token>, ParseErr> {
                     },
                     b'*' => TokenType::Operation(OpType::Mul),
                     b'/' => TokenType::Operation(OpType::Div),
+                    b'^' => TokenType::Operation(OpType::Pow),
+                    b'(' => TokenType::Operation(OpType::LeftParen),
+                    b')' => TokenType::Operation(OpType::RightParen),
                     _ => unreachable!(),
                 },
                 pos: begin+i,
@@ -120,51 +133,100 @@ enum SyntaxErr {
     None,
     MissingArg(Token),
     MissingOp(Token, Token),
+    MissingLeftParen(Token),
+    MissingRightParen(Token),
+    EmptyParens(Token),
 }
 
 fn binary_op_check(pos: usize, tokens: &Vec<Token>) -> Result<(), Token> {
-    if pos == 0 || pos == tokens.len()-1 
-        || is_operation(&tokens[pos-1]) {
+    if pos == 0 || pos == tokens.len()-1 {
         return Err(tokens[pos].clone());
-    } else {
-        match tokens[pos+1].ttype {
-            TokenType::Operation(OpType::UnaryPlus) => {},
-            TokenType::Operation(OpType::UnaryMinus) => {},
-            TokenType::Number(_) => {},
-            _ => return Err(tokens[pos].clone()),
-        }
+    }
+    match tokens[pos-1].ttype {
+        TokenType::Operation(OpType::RightParen) |
+        TokenType::Number(_) => {},
+        _ => return Err(tokens[pos].clone()),
+    }
+    match tokens[pos+1].ttype {
+        TokenType::Operation(OpType::UnaryPlus)  |
+        TokenType::Operation(OpType::UnaryMinus) |
+        TokenType::Operation(OpType::LeftParen)  | TokenType::Number(_) => {},
+        _ => return Err(tokens[pos].clone()),
     }
     Ok(())
 }
 
 fn syntax_check(tokens: &Vec<Token>) -> SyntaxErr {
+    let mut parens = vec![];
     for (i, Token {ttype, ..}) in tokens.iter().enumerate() {
         match ttype {
             TokenType::Operation(OpType::Add) |
             TokenType::Operation(OpType::Sub) |
             TokenType::Operation(OpType::Mul) |
-            TokenType::Operation(OpType::Div) => {
+            TokenType::Operation(OpType::Div) |
+            TokenType::Operation(OpType::Pow) => {
                 if let Err(token) = binary_op_check(i, tokens) {
                     return SyntaxErr::MissingArg(token);
                 }
             },
             TokenType::Operation(OpType::UnaryPlus) |
             TokenType::Operation(OpType::UnaryMinus) => {
-                if i == tokens.len()-1 {
+                if i == tokens.len()-1 || !is_number(&tokens[i+1]) {
                     unreachable!("Probably error in parse()");
-                } else {
-                    match tokens[i+1].ttype {
-                        TokenType::Number(_) => {},
-                        _ => unreachable!("Probably error in parse()"),
-                    }
                 }
             },
-            TokenType::Number(_) => {
+            TokenType::Operation(OpType::LeftParen) => {
+                if i != 0 {
+                    match tokens[i-1].ttype {
+                        TokenType::Operation(OpType::RightParen) |
+                        TokenType::Number(_) => return SyntaxErr::MissingOp(tokens[i-1].clone(), tokens[i].clone()),
+                        _ => {},
+                    }
+                }
+                if i == tokens.len()-1 {
+                    return SyntaxErr::MissingRightParen(tokens[i].clone());
+                }
+                match tokens[i+1].ttype {
+                    TokenType::Operation(OpType::UnaryPlus)  |
+                    TokenType::Operation(OpType::UnaryMinus) |
+                    TokenType::Operation(OpType::LeftParen)  |
+                    TokenType::Number(_) => {},
+                    TokenType::Operation(OpType::RightParen) => return SyntaxErr::EmptyParens(tokens[i].clone()),
+                    _ => return SyntaxErr::MissingArg(tokens[i+1].clone()),
+                }
+                parens.push(&tokens[i]);
+            },
+            TokenType::Operation(OpType::RightParen) => {
+                if i == 0 {
+                    return SyntaxErr::MissingLeftParen(tokens[i].clone());
+                }
+                match tokens[i-1].ttype {
+                    TokenType::Operation(OpType::RightParen) |
+                    TokenType::Number(_) => {},
+                    _ => return SyntaxErr::MissingArg(tokens[i-1].clone()),
+                }
                 if i < tokens.len()-1 && !is_operation(&tokens[i+1]) {
                     return SyntaxErr::MissingOp(tokens[i].clone(), tokens[i+1].clone());
                 }
+                if parens.len() > 0 {
+                    parens.pop();
+                } else {
+                    return SyntaxErr::MissingLeftParen(tokens[i].clone());
+                }
+            },
+            TokenType::Number(_) => {
+                if i < tokens.len()-1 {
+                    match tokens[i+1].ttype {
+                        TokenType::Number(_) |
+                        TokenType::Operation(OpType::LeftParen) => return SyntaxErr::MissingOp(tokens[i].clone(), tokens[i+1].clone()),
+                        _ => {},
+                    }
+                }
             },
         }
+    }
+    if parens.len() > 0 {
+        return SyntaxErr::MissingRightParen(parens[0].clone());
     }
     SyntaxErr::None
 }
@@ -173,7 +235,9 @@ fn op_priority(op: &OpType) -> u8 {
     match op {
         OpType::Add | OpType::Sub => 1,
         OpType::Mul | OpType::Div => 2,
-        OpType::UnaryPlus | OpType::UnaryMinus => 3,
+        OpType::Pow => 3,
+        OpType::UnaryPlus | OpType::UnaryMinus => 4,
+        OpType::LeftParen | OpType::RightParen => panic!("Parentheses has no priority and must be handled differently"),
     }
 }
 
@@ -203,12 +267,19 @@ fn apply_op(numbers: &mut Vec<f64>, op: OpType) {
             let b = numbers.pop().unwrap();
             numbers.push(b / a);
         },
+        OpType::Pow => {
+            assert!(numbers.len() >= 2);
+            let a = numbers.pop().unwrap();
+            let b = numbers.pop().unwrap();
+            numbers.push(b.powf(a));
+        }
         OpType::UnaryPlus => { assert!(numbers.len() > 0) },
         OpType::UnaryMinus => {
             assert!(numbers.len() > 0);
             let a = numbers.pop().unwrap();
             numbers.push(-a);
         },
+        OpType::LeftParen | OpType::RightParen => panic!("Parentheses must be handled differently"),
     }
 }
 
@@ -218,12 +289,21 @@ fn evaluate(tokens: &Vec<Token>) -> f64 {
     for token in tokens {
         match &token.ttype {
             TokenType::Number(num) => numbers.push(*num),
+            TokenType::Operation(OpType::LeftParen) => operations.push(OpType::LeftParen),
+            TokenType::Operation(OpType::RightParen) => {
+                while let Some(last) = operations.pop() {
+                    if last == OpType::LeftParen {
+                        break;
+                    }
+                    apply_op(&mut numbers, last);
+                }
+            },
             TokenType::Operation(op) => {
                 if let None = operations.last() {
                     operations.push(op.clone());
                 } else {
                     while let Some(last) = operations.pop() {
-                        if op_priority(&last) < op_priority(&op) {
+                        if last == OpType::LeftParen || op_priority(&last) < op_priority(&op) {
                             operations.push(last);
                             break
                         }
@@ -325,7 +405,19 @@ fn main() {
             SyntaxErr::MissingOp(a, b) => {
                 print_err(&input, &format!("Missing operation between '{:?}' and '{:?}'", a.ttype, b.ttype), b.pos-1);
                 continue;
-            }
+            },
+            SyntaxErr::MissingRightParen(token) => {
+                print_err(&input, &format!("Missing right paren"), token.pos);
+                continue;
+            },
+            SyntaxErr::MissingLeftParen(token) => {
+                print_err(&input, &format!("Missing left paren"), token.pos);
+                continue;
+            },
+            SyntaxErr::EmptyParens(token) => {
+                print_err(&input, &format!("Empty parentheses"), token.pos);
+                continue;
+            },
         }
 
         let answer = evaluate(&tokens);
